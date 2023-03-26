eafe0047255f1d307e87ae1b3e250bc97a8ccde0
willian.melo <willian.melo@elotech.com.br>
2023-03-26 00:39:21 -0300

    alterando nome de pasta e removendo pasta desnecessaria

---
+++ src/banco-do-brasil/config/AxiosBancoDoBrasil.ts
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+
+const endPointHomologacao = 'https://api.hm.bb.com.br/cobrancas/v2'
+// const endPointProdução = 'https://api.bb.com.br/cobrancas/v2'
+
+
+const apiBancoDoBrasil = axios.create();
+
+
+apiBancoDoBrasil.defaults.baseURL = endPointHomologacao;
+apiBancoDoBrasil.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+
+
+
+export default apiBancoDoBrasil;
---
+++ src/banco-do-brasil/enums/TipoDesconto.ts
@@ -0,0 +1,6 @@
+export enum TipoDesconto {
+  Sem_desconto = 0,
+  Valor_fixo_até_a_data = 1,   
+  Percentual_até_a_data_informada = 2,  
+  Desconto_por_dia_de_antecipação = 3
+}

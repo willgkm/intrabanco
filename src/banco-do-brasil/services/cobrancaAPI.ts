2e877c7d6a39e22c382dcbac2a9d621c8b68ff9f
willian.melo <willian.melo@elotech.com.br>
2023-03-26 02:10:35 -0300

    metodos de listagem, registro e detalhes de boletos

--- src/banco-do-brasil/services/cobrancaAPI.ts
+++ src/banco-do-brasil/services/cobrancaAPI.ts
@@ -1,18 +1,45 @@
 import { AxiosPromise } from "axios";
 import apiBancoDoBrasil from "../config/AxiosBancoDoBrasil";
 import { AlteraBoleto } from "../types/AlteraBoleto";
+import { RequisicaoListaBoleto } from "../types/RequisicaoListaBoletos";
+import { RequisicaoRegistroBoleto } from "../types/RequisicaoRegistraBoleto";
+import { RespostaDetalhesBoleto } from "../types/RespostaDetalhesBoleto";
+import { RespostaListaBoletos } from "../types/RespostaListaBoletos";
+import { RespostaRegistraBoleto } from "../types/RespostaRegistraBoleto";
 
-export default class CobrancaAPI {
+export default class CobrancaApiService {
+  async listaBoletos(
+    queryParams: RequisicaoListaBoleto
+  ): AxiosPromise<Array<RespostaListaBoletos>> {
+    return await apiBancoDoBrasil.get(`/boletos`, {
+      params: queryParams,
+    });
+  }
 
+  async registaBoleto(
+    queryParams: RequisicaoRegistroBoleto
+  ): AxiosPromise<RespostaRegistraBoleto> {
+    return await apiBancoDoBrasil.post(`/boletos`, {
+      params: queryParams,
+    });
+  }
 
-  
+  async detalhesBoleto(
+    pathParams: any,
+    numeroConvenio: number
+  ): AxiosPromise<RespostaDetalhesBoleto> {
+    return await apiBancoDoBrasil.get(`/boletos/${pathParams.id}`, {
+      params: {numeroConvenio: numeroConvenio},
+    });
+  }
+
   async alteraBoleto(
-    params: any,
-    alteraBoleto: AlteraBoleto
+    pathParams: any,
+    body: AlteraBoleto
   ): AxiosPromise<AlteraBoleto> {
-    return await apiBancoDoBrasil.patch(`/boletos/${params.id}`, {
+    return await apiBancoDoBrasil.patch(`/boletos/${pathParams.id}`, {
       params: {},
-      data: alteraBoleto,
+      data: body,
     });
   }
 }
